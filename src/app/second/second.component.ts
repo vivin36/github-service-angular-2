b17d67aeb1afb854693c0d5e50b71f847c30c5c1
vivin36 <vivinprasannaa@outlook.com>
2017-07-23 22:14:22 +0530

    first

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {GithubservicesService} from '../github/githubservices.service';
 import * as moment from 'moment';
-
+declare let jsPDF;
+import {Pipe, PipeTransform} from '@angular/core';
 
 
 
@@ -43,9 +44,38 @@ export class SecondComponent implements OnInit {
   			this.result = res;
   			console.log('res',res);
   		});
-  	
-  
+  	  }
 
+
+        convert(){
+
+
+      const elementToPrint = document.getElementById('div1');
+      console.log('eelementToPrint',elementToPrint); //The html element to become a pdf
+      const pdf = new jsPDF('p', 'pt', 'a4');
+      pdf.addHTML(elementToPrint, () => {
+          pdf.save('web.pdf');
+      });
+
+
+    var item = {
+      "Name" : "XYZ",
+      "Age" : "22",
+      "Gender" : "Male"
+    };
+    var doc = new jsPDF();
+    var col = ["Details", "Values"];
+    var rows = [];
+
+    for(var key in item){
+        var temp = [key, item[key]];
+        rows.push(temp);
+    }
+
+    doc.autoTable(col, rows);
+
+    doc.save('Test.pdf');
   }
+
 
 }
--- src/app/third-sample/third-sample.component.ts
+++ src/app/third-sample/third-sample.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Injectable }     from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-third-sample',
   templateUrl: './third-sample.component.html',
   styleUrls: ['./third-sample.component.css']
 })
-export class ThrdSampleComponent implements OnInit {
+export class ThrdSampleComponent {
+testResponse:any;
+publicDeals: any[] = [];
+subMenu:any[] = [];
+formatted:any[] = [];
+includesword:string ='sample';
 
-  constructor() { }
+     constructor(private http: Http) {
 
-  ngOnInit() {
-  }
+		 this.getMenu().subscribe(
+        data => {
+          this.testResponse = data;
+          console.log("I CANT SEE DATA HERE: ", this.testResponse.data.length);
+          this.getDetails(this.testResponse,this.testResponse.data.length);
+        }
+    );
+    }
+
+  getMenu() {
+    return this.http.get('https://api.myjson.com/bins/lm1wz')
+        .map(data => {
+            data.json();
+            return data.json();
+    });
+}
+
+	getDetails(res,len){
+		this.formatted = [];
+		for(let i = 0 ;i<len;i++){
+			
+			
+			if(res.data[i].submenu !== null){
+				for(let j=0;j<len;j++){
+					console.log(j,res.data[i].submenu,res.data[j].submenu);
+					if(res.data[j].submenu !== null )
+					if(res.data[i].submenu  == res.data[j].submenu && !(this.includesword.includes(res.data[j].submenu))){
+						this.subMenu.push({"res":res.data[j].menu_name});
+					}
+
+				}
+				if(this.subMenu.length > 0)
+				this.formatted.push({"reportName":res.data[i].submenu,"SubMenu":this.subMenu}) ;
+				this.includesword = res.data[i].submenu+',';
+				this.subMenu= [];
+				console.log(this.formatted,'this.formatted',res.data[i],this.includesword,this.includesword.includes('outdoo'));
+			}
+			else if((res.data[i].submenu == null)) {
+				this.formatted.push({"reportName":res.data[i].menu_name}) 
+				
+			}
+
+
+
+		}
+	}
 
 }
